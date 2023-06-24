e8158c85d2fd09fdc31e6bc3f1f8df1e2be17226
Rimal Parikh <parikhr8@gmail.com>
2023-06-24 14:24:38 +0530

    styling changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./styles.css";
 import React from "react";
 import essays from "./data/pg-essays.json";
 import UnorderedList from "./components/articles/UnorderedList";
+import { PG_WEB_URL } from "./constants";
 
 const App = () => {
   return (
     <div className="container">
       <main>
-        <h1>Paul Graham Essays</h1>
+        <h1><a href={PG_WEB_URL}>Paul Graham Essays</a></h1>
         <UnorderedList data={essays} />
       </main>
     </div>
